92551cfc777474bb24bd5fcdb91a69f69af0d4b1
0dividedby0 <jason.halcomb@wsu.edu>
2026-04-18 14:10:34 -0700

    Removing unused import

--- src/main/java/com/dividedby0/victorymod/StructureSpawner.java
+++ src/main/java/com/dividedby0/victorymod/StructureSpawner.java
@@ -2,7 +2,6 @@ package com.dividedby0.victorymod;
 
 import com.dividedby0.victorymod.config.ConfigManager;
 import com.dividedby0.victorymod.config.JSON5ConfigManager;
-import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import net.minecraft.core.BlockPos;
